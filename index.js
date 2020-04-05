c54e52ae0eb97eb3078c1ae529ca2f71119165cd
Corey Hunter <coreymadcreationapps@gmail.com>
2020-04-05 14:56:32 +0700

    Completed the calculation. All logic and variables are working.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var bodyweight = 100.8;
+var bodyweight = 100;
 console.log("This is the user's bodyweight " + bodyweight)
 var height = 1.91
 console.log("This is the user's bodyweight " + height)
@@ -8,37 +8,26 @@ console.log("This is the user's height to the second power " + m2)
 var userBMI = bodyweight/m2;
 console.log("This is the User's dirty BMI " + userBMI)
 
-
-
-
-
-
-function categorizeUser(bodyweight,height, userBMI) {
-
-var category = "Underweight";
 //Underweight <18.5
 if (userBMI < 18.5){
-  category = "Underweight"
+category = "Underweight"
 //Normal 18.5-24.9
 }
-if (userBMI > 18.6 && userBMI < 24.9) {
-  category = "Normal"
+else if(userBMI > 18.6 && userBMI < 24.9) {
+category = 'Normal'
 }
 //Overweight 25-29
-if (userBMI > 25 && userBMI < 29.9) {
-  category = "Overweight"
+else if (userBMI > 25 && userBMI < 29.9) {
+category = 'Overweight'
 }
 //Obese 30-34.9
-if (userBMI > 30 && userBMI < 34.9) {
-  category = "Obese"
+else if (userBMI > 30 && userBMI < 34.9) {
+category = 'Obese'
 }
 //Extremely Obese 35+
-if (userBMI > 35){
-  category = "Extremely Obese"
-}
-return category;
-
+else if (userBMI > 35){
+category = 'Extremely Obese'
 }
 
-categorizeUser();
-alert("Your BMI is " + userBMI  + "You are " + category);
+
+alert("Your BMI is " + userBMI +"." + " You are " + category+".");
